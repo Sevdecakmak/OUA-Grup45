49c0ef3f8ba048325bc1e3f10b17bb3a4e3f1f7c
Ridvanks01 <karasubasiridvan@gmail.com>
2024-07-23 10:00:12 +0300

    bulmacanın iskeleti oluşturuldu

---
+++ Assets/Scripts/answer_checker.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class answer_checker : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Question
+    {
+        public Image firstEmoji;
+        public Image secondEmoji;
+        public Image answerImage;
+        public Image crossImageTik;
+        public Image crossImageCarpi;
+
+        public Sprite correctAnswer;
+        public Sprite image1;
+        public Sprite image2;
+    }
+
+    public Question[] questions; // Sorular
+    public Button checkButton;
+
+    void Start()
+    {
+        checkButton.onClick.AddListener(CheckAnswers);
+        foreach (var question in questions)
+        {
+            question.crossImageTik.gameObject.SetActive(false); // Başlangıçta çarpı işaretlerini gizle
+            question.crossImageCarpi.gameObject.SetActive(false);
+        }
+    }
+
+    void CheckAnswers()
+    {
+        bool allCorrect = true;
+        foreach (var question in questions)
+        {
+            if (question.answerImage.sprite == question.correctAnswer)
+            {
+                question.crossImageTik.gameObject.SetActive(true); // Cevap doğruysa çarpı işaretini gizle
+                question.crossImageCarpi.gameObject.SetActive(false);
+            }
+            else
+            {
+                question.crossImageCarpi.gameObject.SetActive(true); // Cevap yanlışsa çarpı işaretini göster
+                question.crossImageTik.gameObject.SetActive(false);
+                allCorrect = false;
+            }
+        }
+
+        if (allCorrect)
+            Debug.Log("Hepsi Doğru!");
+        else
+            Debug.Log("Hata var!");
+    }
+}
---
+++ Assets/Scripts/emoji_changer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class emoji_changer : MonoBehaviour, IPointerClickHandler
+{
+    public Image answerImage;
+    public Sprite[] possibleAnswers; // Farklı emoji görselleriniz
+
+    private int currentAnswerIndex = 0;
+
+    void Start()
+    {
+        if (possibleAnswers.Length > 0)
+        {
+            answerImage.sprite = possibleAnswers[currentAnswerIndex];
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ChangeAnswer();
+    }
+
+    void ChangeAnswer()
+    {
+        currentAnswerIndex = (currentAnswerIndex + 1) % possibleAnswers.Length;
+        answerImage.sprite = possibleAnswers[currentAnswerIndex];
+    }
+}
