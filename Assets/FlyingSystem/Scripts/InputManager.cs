d58e4a081fbdaddda5c247721680e949436f226e
wolfscatt <bjkeses11@hotmail.com>
2024-07-30 09:47:25 +0300

    FlyingSystem Added * Character will be added. * Attack and other animations will be added. * Attack script will be written.

---
+++ Assets/FlyingSystem/Scripts/InputManager.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private CrowControls crowControls;
+    private AnimatorManager animatorManager;
+    private CrowLocomotion crowController;
+    public Vector2 movementInput;
+    public Vector2 cameraInput;
+
+    public float cameraInputX;
+    public float cameraInputY;
+
+    public float moveAmount;
+    public float verticalInput;
+    public float horizontalInput;
+
+    public bool b_Input;
+    public bool x_Input;
+    public bool jump_Input;
+    public bool fly_Input;
+
+
+    public delegate void MouseLeftClickAction();
+    public static event MouseLeftClickAction OnMouseLeftClick;
+
+    public delegate void EscapeAction();
+    public static event EscapeAction OnEscape;
+    private void Awake() 
+    {
+        animatorManager = GetComponent<AnimatorManager>();
+        crowController = GetComponent<CrowLocomotion>();
+    }
+
+    private void OnEnable() 
+    {
+        if(crowControls == null)
+        {
+            crowControls = new CrowControls();
+            
+            // Buradaki Movement ve Camera aksiyonları pass through olduğu için değerlerini alabiliyoruz.
+            crowControls.CrowMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+            crowControls.CrowMovement.Camera.performed += i => cameraInput = i.ReadValue<Vector2>();
+
+            // Buradaki Click ve Sprint aksiyonları ise bir Button.
+            crowControls.CrowMovement.Click.performed += i => OnClickPerformed(i);
+            crowControls.CrowMovement.Click.canceled += i => OnClickCanceled(i);
+
+            // Crow Controls inputlarından CrowActions içerisindeki sprint inputuna basıldığında b_Input true olacak.
+            crowControls.CrowActions.Sprint.performed += i => b_Input = true;
+            crowControls.CrowActions.Sprint.canceled += i => b_Input = false;
+
+            // Crow Controls inputlarından CrowActions içerisindeki jump inputuna basıldığında jump_Input true olacak.
+            crowControls.CrowActions.Jump.performed += i => jump_Input = true;
+
+            // Dodgeleme geri sıçrama hareketi için x_Input true olacak.
+            crowControls.CrowActions.X.performed += i => x_Input = true;
+
+            // Uçma işlemi için fly_Input true olacak.
+            crowControls.CrowActions.Fly.performed += i => fly_Input = true;
+            crowControls.CrowActions.Fly.canceled += i => fly_Input = false;
+        }
+        crowControls.Enable();
+    }
+
+    private void OnDisable() 
+    {
+        crowControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        HandleSprintingInput();
+        HandleJumpingInput();
+        HandleDodgeInput();
+        HandleFlyingInput();
+        // HandleAttackInput();
+    }
+    private void HandleMovementInput()
+    {
+        horizontalInput = movementInput.x;
+        verticalInput = movementInput.y;
+
+        cameraInputX = cameraInput.x;
+        cameraInputY = cameraInput.y;
+
+        moveAmount = Mathf.Clamp01(Mathf.Abs(horizontalInput) + Mathf.Abs(verticalInput));
+        animatorManager.UpdateAnimatorValues(0, moveAmount, crowController.isSprinting);
+    }
+    private void HandleSprintingInput()
+    {
+        if(b_Input && moveAmount > 0.5f)
+        {
+            crowController.isSprinting = true;
+        }
+        else
+        {
+            crowController.isSprinting = false;
+        }
+    }
+
+    private void HandleJumpingInput()
+    {
+        if(jump_Input)
+        {
+            jump_Input = false;
+            crowController.HandleJumping();
+        }
+    }
+    private void HandleDodgeInput()
+    {
+        if(x_Input)
+        {
+            x_Input = false;
+            crowController.HandleDodge();
+        }
+    }
+
+    private void HandleFlyingInput()
+    {
+        if(fly_Input)
+        {
+            crowController.HandleFlying(); // Uçma işlemi için yeni bir metod eklenmeli
+        }
+    }
+
+    private void OnClickPerformed(InputAction.CallbackContext context)
+    {
+        if (context.control.name == "leftButton")
+        {
+            OnMouseLeftClick?.Invoke();
+        }
+    }
+    private void OnClickCanceled(InputAction.CallbackContext context)
+    {
+        if (context.control.name == "escape")
+        {
+            OnEscape?.Invoke();
+        }
+    }
+}
